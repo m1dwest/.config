68ef1a7d4a30ac38d986574c5232da5d384dc60a
Roman Peterburgskii <waveformspb@gmail.com>
2022-10-04 12:33:17 +0300

    Add more configs

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+alias r="ranger-cd"
+
+set CC clang-11
+set CXX clang++-11
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+function fish_greeting
+end
+
+fish_vi_key_bindings
+
+fish_add_path /usr/local/go/bin 
+fish_add_path /home/midwest/go/bin
